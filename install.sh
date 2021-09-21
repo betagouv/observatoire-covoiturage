86dad516f86f2ccc088731fbdc5d35b29c1fe423
Nicolas Mérigot <nicolas@belenio.fr>
2021-09-21 15:24:46 +0200

    feat: add install & entrypoint scripts

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Install api"
+(cd api && npm ci && npm run build)
+
+echo "Build frontend"
+(cd observatoire && npm ci && NODE_ENV=production NUXT_TELEMETRY_DISABLED=1 npm run generate && [[ ! -z "$1" ]] && cp -R dist $1)
