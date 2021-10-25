36c6b5f67fef29f223f2df3cacad95001f0930a4
Nicolas Mérigot <nicolas@belenio.fr>
2021-10-25 10:36:58 +0200

    fix: copy folder content

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ echo "Install api"
 (cd api && npm ci && npm run build)
 
 echo "Build frontend"
-(cd observatoire && npm ci && NODE_ENV=production NUXT_TELEMETRY_DISABLED=1 npm run generate && [ ! -z "$PUBLIC_DIR" ] && cp -R dist $PUBLIC_DIR || exit 0)
+(cd observatoire && npm ci && NODE_ENV=production NUXT_TELEMETRY_DISABLED=1 npm run generate && [ ! -z "$PUBLIC_DIR" ] && cp -R dist/* $PUBLIC_DIR || exit 0)
+
