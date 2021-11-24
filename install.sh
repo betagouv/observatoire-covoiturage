e435a8679751a0c72db40c460a326274f4ddf947
Nicolas Mérigot <nicolas@belenio.fr>
2021-11-24 16:54:55 +0100

    fix: public directory

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ echo "Install api"
 (cd api && npm ci && npm run build)
 
 echo "Build frontend"
-(cd observatoire && npm ci && NODE_ENV=production NUXT_TELEMETRY_DISABLED=1 npm run generate && [ ! -z "$PUBLIC_DIR" ] && cp -R dist/* $PUBLIC_DIR || exit 0)
+(cd observatoire && npm ci && NODE_ENV=production NUXT_TELEMETRY_DISABLED=1 npm run generate && [ ! -z "$1" ] && cp -R dist/* $1 || exit 0)
 
--- postdeploy.sh
+++ postdeploy.sh
@@ -4,4 +4,4 @@ cd /home/node/app
 PUBLIC_DIR="$(pwd)/../public"
 git fetch
 git merge $1
-./install.sh
+./install.sh $PUBLIC_DIR
