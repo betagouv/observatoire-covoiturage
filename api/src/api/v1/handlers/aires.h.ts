c1821f77dc0e96554e8e9de3a6f39ed183d8a50b
Ludovic Delhomme <ludovic.delhomme@beta.gouv.fr>
2021-12-07 17:55:46 +0100

    fix table name error in aires_covoiturage

--- api/src/api/v1/handlers/aires.h.ts
+++ api/src/api/v1/handlers/aires.h.ts
@@ -7,7 +7,20 @@ export default class airesHandler {
   static async all(request: FastifyRequest, reply: FastifyReply):Promise<void>{
     try {
       const client = await this.pg.connect()
-      const sql = `SELECT id_lieu, ad_lieu, com_lieu, type, date_maj, nbre_pl, nbre_pmr, duree, horaires, proprio, lumiere, comm, ST_AsGeoJSON(geom,6)::json as geom FROM covoiturage.aires;`
+      const sql = `SELECT id_lieu, 
+        ad_lieu, 
+        com_lieu, 
+        type, 
+        date_maj, 
+        nbre_pl, 
+        nbre_pmr, 
+        duree, 
+        horaires, 
+        proprio, 
+        lumiere, 
+        comm, 
+        ST_AsGeoJSON(geom,6)::json as geom
+        FROM aires_covoiturage;`
       const {rows} = await client.query(sql)
       if (!rows || rows.length === 0) {
         reply.code(404).send(new Error('data not found'))
