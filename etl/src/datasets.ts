d2e714c67a1f69c9e20dc400c1a077a476381eaa
Ludovic Delhomme <ludovic.delhomme@beta.gouv.fr>
2022-02-15 13:53:42 +0100

    add perimeters 2022 and rpc data 01/2022

--- etl/src/datasets.ts
+++ etl/src/datasets.ts
@@ -1,12 +1,15 @@
 import { datasets as perimDatasets, StaticMigrable } from '@betagouvpdc/evolution-geo';
+import { Perim2022 } from './datasets/manual/Perim2022';
 import { CreateTerritoriesCodeTable } from './datastructure/000_CreateTerritoriesCodeTable';
 import { CreateTerritoriesPointTable } from './datastructure/001_CreateTerritoriesPointTable';
 import { CreateAiresCovoiturageTable } from './datastructure/002_CreateAiresCovoiturageTable';
 import { CreateRpcTable } from './datastructure/003_CreateRpcTable';
 import { TerritoriesCode2020 } from './datasets/territories/codes/TerritoriesCode2020';
 import { TerritoriesCode2021 } from './datasets/territories/codes/TerritoriesCode2021';
+import { TerritoriesCode2022 } from './datasets/territories/codes/TerritoriesCode2022';
 import { TerritoriesPoint2020 } from './datasets/territories/points/TerritoriesPoint2020';
 import { TerritoriesPoint2021 } from './datasets/territories/points/TerritoriesPoint2021';
+import { TerritoriesPoint2022 } from './datasets/territories/points/TerritoriesPoint2022';
 import { CreateMonthlyFluxTable } from './datastructure/004_CreateMonthlyFluxTable';
 import { CreateMonthlyOccupationTable } from './datastructure/005_CreateMonthlyOccupationTable';
 import { TransportAires2021 } from './datasets/transport_data_gouv/aires_covoiturage/2021/TransportAires2021';
@@ -16,12 +19,15 @@ import { Datasets } from './interfaces/DatasetsInterface';
 
 export async function datasets(): Promise<Datasets> {
   const datasets: Set<StaticMigrable> = perimDatasets.datasets;
+  datasets.add(Perim2022);
   datasets.add(CreateTerritoriesCodeTable);
   datasets.add(TerritoriesCode2020);
   datasets.add(TerritoriesCode2021);
+  datasets.add(TerritoriesCode2022);
   datasets.add(CreateTerritoriesPointTable);
   datasets.add(TerritoriesPoint2020);
   datasets.add(TerritoriesPoint2021);
+  datasets.add(TerritoriesPoint2022);
   datasets.add(CreateAiresCovoiturageTable);
   datasets.add(TransportAires2021);
   datasets.add(CreateRpcTable);
---
+++ etl/src/datasets/territories/points/TerritoriesPoint2022.spec.ts
@@ -0,0 +1,68 @@
+import anyTest, { TestInterface } from 'ava';
+import { Pool } from 'pg';
+import { MemoryStateManager } from '@betagouvpdc/evolution-geo/dist/providers';
+import { Migrator, DatasetInterface } from '@betagouvpdc/evolution-geo';
+import { createPool, createFileManager } from '@betagouvpdc/evolution-geo/dist/helpers/';
+import { CreateTerritoriesPointTable } from '../../../datastructure/001_CreateTerritoriesPointTable';
+import { TerritoriesPoint2022 } from './TerritoriesPoint2022';
+
+interface TestContext {
+  migrator: Migrator;
+  connection: Pool;
+  dataset: DatasetInterface;
+}
+
+const test = anyTest as TestInterface<TestContext>;
+const Dataset = TerritoriesPoint2022;
+const table = TerritoriesPoint2022.table;
+test.before(async (t) => {
+  t.context.connection = createPool({
+    user: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
+    database: process.env.POSTGRES_DB || 'local',
+    host: process.env.POSTGRES_HOST || 'postgres',
+    port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT, 10) : 5432,
+  });
+  t.context.migrator = new Migrator(t.context.connection, createFileManager(), {
+    targetSchema: 'public',
+    migrations: new Set([CreateTerritoriesPointTable, Dataset]),
+    noCleanup: false,
+  });
+  t.context.dataset = new Dataset(t.context.connection, createFileManager(), 'public');
+  await t.context.connection.query(`
+    DROP TABLE IF EXISTS ${table}
+  `);
+  await t.context.migrator.prepare();
+});
+
+test.after.always(async (t) => {
+  await t.context.connection.query(`
+    DROP TABLE IF EXISTS ${table}
+  `);
+});
+
+test.serial('should validate', async (t) => {
+  await t.notThrowsAsync(() => t.context.dataset.validate(new MemoryStateManager()));
+});
+
+test.serial('should prepare', async (t) => {
+  await t.notThrowsAsync(() => t.context.dataset.before());
+});
+
+test.serial('should load', async (t) => {
+  await t.notThrowsAsync(() => t.context.dataset.load());
+});
+
+test.serial('should import', async (t) => {
+  await t.context.migrator.run();
+  const response = await t.context.connection.query(`
+      SELECT count(*) FROM (SELECT 1 FROM ${table} LIMIT 1) AS t;
+    `);
+  t.is(response.rows[0].count, '1');
+});
+
+test.serial('should cleanup', async (t) => {
+  await t.context.dataset.after();
+  const query = `DELETE FROM ${table} WHERE year = 2022`;
+  await t.throwsAsync(() => t.context.connection.query(query));
+});
