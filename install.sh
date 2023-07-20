42a8a3e6607431038ebd34f0dcda2a8c19675694
Jonathan Fallon <jonathan@scopopop.com>
2023-07-21 00:33:40 +0200

    ci

--- install.sh
+++ install.sh
@@ -6,5 +6,4 @@ echo "Install api"
 (cd api && npm ci && npm run build)
 
 echo "Build frontend"
-(cd observatoire && npm ci && NODE_ENV=production NUXT_TELEMETRY_DISABLED=1 npm run generate && [ ! -z "$1" ] && cp -R dist/* $1 || exit 0)
-
+(cd observatoire && npm ci && NODE_OPTIONS=--openssl-legacy-provider NODE_ENV=production NUXT_TELEMETRY_DISABLED=1 npm run generate && [ ! -z "$1" ] && cp -R dist/* $1 || exit 0)
