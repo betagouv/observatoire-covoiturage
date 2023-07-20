b78d4c5f035a10e0f5d00dd8a80481a2c8561cb3
Jonathan Fallon <jonathan@scopopop.com>
2023-07-21 00:43:03 +0200

    update npm

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ echo "Install api"
 (cd api && npm ci && npm run build)
 
 echo "Build frontend"
+node -v
+npm -v
 (cd observatoire && npm ci && NODE_OPTIONS=--openssl-legacy-provider NODE_ENV=production NUXT_TELEMETRY_DISABLED=1 npm run generate && [ ! -z "$1" ] && cp -R dist/* $1 || exit 0)
--- postdeploy.sh
+++ postdeploy.sh
@@ -4,4 +4,5 @@ cd /home/node/app
 PUBLIC_DIR="$(pwd)/../public"
 git fetch
 git merge $1
+npm install -g npm@latest
 ./install.sh $PUBLIC_DIR
